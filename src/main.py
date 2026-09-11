4b9f320122368448815c74d1deeed85bf9e46cbd
DLKY <s240599@dtu.dk>
2026-09-12 00:56:10 +0200

    Add package weight check for transport conveyor

--- src/main.py
+++ src/main.py
@@ -1,1 +1,6 @@
-print("hello!")
+antal_motor = int(input()) 
+pakke_vaegt = int(input()) 
+if(pakke_vaegt // antal_motor <= 12):
+    print("Ja! Transportbåndet kan transportere pakkerne.") 
+else:
+    print("Nej! Transportbåndet kan ikke transportere pakkerne.")
